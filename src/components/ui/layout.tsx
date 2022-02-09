52f1b6825ae2ab24633a3400feae7cfbfe54af33
jstaylor926 <54418577+jstaylor926@users.noreply.github.com>
2022-02-08 19:16:31 -0500

    initial commit

---
+++ src/components/ui/layout.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout = ({children}: LayoutProps) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default Layout;
